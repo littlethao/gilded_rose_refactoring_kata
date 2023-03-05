cd488e32754b998c2aa21777cbc3186b7bbbf7b2
Thao Vo <thao.vo@junipereducation.org>
2023-03-05 21:58:19 +0000

    Extract methods to increase/decrease quality
    Another small refactor we can make at this stage is to extract methods
    for when we increase or decrease the quality value by one, which makes
    it easier to read. For now, we are keeping the decrease of sell in value
    as is and move it higher up since this is applied only once and
    happening to any item.

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -8,45 +8,55 @@ class GildedRose
     @items.each do |item|
       return if item.name == "Sulfuras, Hand of Ragnaros"
 
+      item.sell_in -= 1
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          item.quality = item.quality - 1
+          decrease_quality_on(item)
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          increase_quality_on(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality_on(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality_on(item)
               end
             end
           end
         end
       end
 
-      item.sell_in = item.sell_in - 1
-
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              item.quality = item.quality - 1
+              decrease_quality_on(item)
             end
           else
             item.quality = item.quality - item.quality
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            increase_quality_on(item)
           end
         end
       end
     end
+  end
+
+  private
+
+  def decrease_quality_on(item)
+    item.quality -= 1
+  end
+
+  def increase_quality_on(item)
+    item.quality += 1
   end
 end

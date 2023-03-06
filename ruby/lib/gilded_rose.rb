2ff6f4d60894f5132e7f5eb9ba688af508e21595
Thao Vo <thao.vo@junipereducation.org>
2023-03-06 08:24:51 +0000

    Reorganise conditions according to item types
    In order to make it easier to follow the many conditions within the
    `#update_quality` method, we are reorganising the conditions according
    to the item types.

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -8,45 +8,34 @@ class GildedRose
     @items.each do |item|
       return if item.name == "Sulfuras, Hand of Ragnaros"
 
-      item.sell_in -= 1
-
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          decrease_quality_on(item)
-        end
-      else
+      if item.name == "Aged Brie"
         if item.quality < 50
           increase_quality_on(item)
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                increase_quality_on(item)
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                increase_quality_on(item)
-              end
-            end
-          end
         end
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              decrease_quality_on(item)
-            end
-          else
-            item.quality = item.quality - item.quality
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality < 50
+          increase_quality_on(item)
+          if item.sell_in < 11
+            increase_quality_on(item)
           end
-        else
-          if item.quality < 50
+          if item.sell_in < 6
             increase_quality_on(item)
           end
         end
+
+        if item.sell_in < 0
+          item.quality = item.quality - item.quality
+        end
+      else
+        if item.quality > 0
+          decrease_quality_on(item)
+          if item.sell_in < 0 
+            decrease_quality_on(item)
+          end
+        end
       end
+
+      item.sell_in -= 1
     end
   end
 
